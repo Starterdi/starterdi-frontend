423daeaee78e7047e4a7c5ac00a6b324aa219a2b
yjs04 <yjsss04@kakao.com>
2021-06-12 12:41:32 +0900

    visual

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './Header';
 import reportWebVitals from './reportWebVitals';
 import './style.scss'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
