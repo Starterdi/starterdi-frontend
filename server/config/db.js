898647516221982a44161f6a44064aecbed7c267
yjs04 <yjsss04@kakao.com>
2021-09-01 16:05:53 +0900

    db connect

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,18 @@
-const mysql = require('mysql');
-const db = mysql.createPool({
+const mysql = require('mysql2');
+let conn = mysql.createConnection({
     host:'test.itsw.info',
     user:'sw202135',
     password:'sw202135',
-    database:'sw202135',
+    database:'sw202135'
 });
 
-module.exports = db;
+
+conn.connect();
+
+conn.query("SELECT user_name FROM User WHERE idx = 1",(err,rows,fileds)=>{
+    if(err) console.log(err);
+    console.log(rows);
+    conn.end();
+})
+
+exports.conn = conn;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,14 @@
 const express =  require('express');
 const app = express();
 // const bodyParser = require('body-parser');
-const port = 3306;
+const port = 3309;
 const cors = require('cors');
 // const route = require('./routes/index');
-const main = require('./routes/index');
-const db = require('./config/db');
+const conn = require('./config/db');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use (express.json());
-
-app.use('/5/',main);
 
 app.listen(port, ()=> {
     console.log(`express is running on ${port}`);
